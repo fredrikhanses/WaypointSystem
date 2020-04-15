b37c561b4381418456ef720c7b6171fd9959f304
Fredrik Hanses <fredrik.hanses@gmail.com>
2020-04-15 21:13:41 +0200

    Working on grid

--- Assets/Editor/SnapperTool.cs
+++ Assets/Editor/SnapperTool.cs
@@ -25,7 +25,47 @@ public class SnapperTool : EditorWindow
 
     private void DuringSceneGUI(SceneView sceneView)
     {
-        Handles.DrawLine(Vector3.zero, Vector3.up);
+        DrawGrid(2);
+    }
+
+    private void DrawGrid(int size)
+    {
+        int initSize = size;
+        Vector3 right = Vector3.zero;
+        Vector3 left = Vector3.zero;
+        Vector3 forward = Vector3.zero;
+        Vector3 back = Vector3.zero;
+        while (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Handles.color = Color.cyan;
+                    Handles.DrawLine(right, Vector3.left + right);
+                    Handles.DrawLine(right, Vector3.forward + right);
+                    Handles.DrawLine(right, Vector3.back + right);
+                    right += Vector3.right;
+                    Handles.color = Color.black;
+                    Handles.DrawLine(left, Vector3.right + left);
+                    Handles.DrawLine(left, Vector3.forward + left);
+                    Handles.DrawLine(left, Vector3.back + left);
+                    left += Vector3.left;
+                    Handles.color = Color.blue;
+                    Handles.DrawLine(forward, Vector3.right + forward);
+                    Handles.DrawLine(forward, Vector3.left + forward);
+                    Handles.DrawLine(forward, Vector3.back + forward);
+                    forward += Vector3.forward;
+                    Handles.color = Color.white;
+                    Handles.DrawLine(back, Vector3.right + back);
+                    Handles.DrawLine(back, Vector3.left + back);
+                    Handles.DrawLine(back, Vector3.forward + back);
+                    back += Vector3.back;
+                }
+            }
+            size--;
+        }
+
     }
 
     private void OnGUI()
