fc380d838d45a5663fb6c07fa112ec74f066bdf0
Fredrik Hanses <fredrik.hanses@gmail.com>
2020-06-22 16:31:08 +0200

    Added buttons that manipulates waypoints.

---
+++ Assets/Scripts/Patroller.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStartMoving
+{
+    void StartMoving(IEnumerable<Vector3> path);
+}
+
+[RequireComponent(typeof(Rigidbody))]
+[SelectionBase]
+public class Patroller : MonoBehaviour, IStartMoving
+{
+    [SerializeField, Range(1f, 100f)] private float m_Speed;
+    [SerializeField] private Rigidbody m_Rigidbody;
+    [SerializeField] public List<GameObject> m_Waypoints;
+
+    private bool m_Move;
+    private bool m_Stop;
+    private float m_OldDistanceSquared = float.MaxValue;
+    private Vector3 m_TargetPosition;
+    private Vector3 m_Direction;
+    private LinkedList<Vector3> m_Path;
+    private LinkedList<Vector3> m_InitialPath;
+
+    private const float k_MinDistanceToGoal = 0.01f;
+
+    private void Start()
+    {
+        if (m_Rigidbody == null)
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+        }
+        m_InitialPath = new LinkedList<Vector3>();
+        m_InitialPath.AddLast(new Vector3(10f, 0f));
+        m_InitialPath.AddLast(new Vector3(10f, 0f, -10f));
+        m_InitialPath.AddLast(new Vector3(0f, 0f, -10f));
+        m_InitialPath.AddLast(new Vector3(-10f, 0f, -10f));
+        m_InitialPath.AddLast(new Vector3(-10f, 0f, 0f));
+        m_InitialPath.AddLast(Vector3.zero);
+        StartMoving(m_InitialPath);
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_Move)
+        {
+            float distanceSquared = (transform.position - m_TargetPosition).sqrMagnitude;
+            if (distanceSquared < (k_MinDistanceToGoal * k_MinDistanceToGoal) || (Mathf.Abs(distanceSquared) > Mathf.Abs(m_OldDistanceSquared)))
+            {
+                m_OldDistanceSquared = float.MaxValue;
+                m_Rigidbody.velocity = Vector3.zero;
+                transform.position = m_TargetPosition;
+                m_Move = false;
+                m_Stop = true;
+            }
+            else
+            {
+                m_OldDistanceSquared = distanceSquared;
+            }
+        }
+        if (m_Stop)
+        {
+            m_Stop = false;
+            MoveToNext();
+        }
+    }
+
+    /// <summary> Start enemy movement along path.</summary>
+    /// <param name="path"> Path to walk along.</param>
+    public void StartMoving(IEnumerable<Vector3> path)
+    {
+        m_Path = new LinkedList<Vector3>(path);
+        if (m_Path.Count > 0)
+        {
+            MoveTo(m_Path.First.Value);
+            m_Path.RemoveFirst();
+        }
+    }
+
+    private void MoveTo(Vector3 targetPosition)
+    {
+        m_TargetPosition = transform.position;
+        m_Direction.x = GetDirection(m_TargetPosition.x, targetPosition.x);
+        m_Direction.z = GetDirection(m_TargetPosition.z, targetPosition.z);
+        Debug.Log(m_Direction);
+        transform.rotation = Quaternion.LookRotation(m_Direction);
+        m_TargetPosition = targetPosition;
+        m_Rigidbody.velocity = m_Direction.normalized * m_Speed;
+        m_Move = true;
+    }
+
+    //Calculate direction.
+    private float GetDirection(float currentPosition, float targetPosition)
+    {
+        if (currentPosition > targetPosition)
+        {
+            return -Mathf.Sqrt(Mathf.Abs(targetPosition * targetPosition - currentPosition * currentPosition));
+        }
+        else if (Mathf.Abs(currentPosition) > Mathf.Abs(targetPosition))
+        {
+            return Mathf.Sqrt(Mathf.Abs(targetPosition * targetPosition - currentPosition * currentPosition));
+        }
+        else if (currentPosition == targetPosition)
+        {
+            return 0f;
+        }
+        else
+        {
+            return Mathf.Sqrt(targetPosition * targetPosition - currentPosition * currentPosition);
+        }
+    }
+
+    private void MoveToNext()
+    {
+        if (m_Path.Count > 0)
+        {
+            MoveTo(m_Path.First.Value);
+            m_Path.RemoveFirst();
+        }
+        else
+        {
+            StartMoving(m_InitialPath);
+        }
+    }
+}
--- Assets/Scripts/WaypointSystemEditor.cs
+++ Assets/Scripts/WaypointSystemEditor.cs
@@ -1,25 +1,58 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class WaypointSystemEditor : EditorWindow
 {
-    public Vector3 point;
-    SerializedObject serializedObject;
-    SerializedProperty propertyPoint;
+    [SerializeField] public Patroller patroller;
 
-    Vector3[] points;
+    [Header("Current Point")]
+    public Vector3 m_PointCurrent = Vector3.zero;
+    [Header("Temporary Points")]
+    public List<Vector3> m_PointsTemporary = new List<Vector3>();
+    [Header("Confirmed Points")]
+    public List<Vector3> m_PointsConfirmed = new List<Vector3>();
+    [Header("Waypoints")]
+    public List<GameObject> m_Waypoints = new List<GameObject>();
+
+    public List<GameObject> Waypoints { get => m_Waypoints; }
+
+    private SerializedObject m_SerializedObject;
+    private SerializedProperty m_PropertyPointCurrent;
+    private SerializedProperty m_PropertyPointsTemporary; 
+    private SerializedProperty m_PropertyPointsConfirmed;
+    private SerializedProperty m_PropertyWaypoints;
+    private int m_ToolbarInt = 0;
+    private string[] m_ToolbarStrings = new string[] { "Toolbar1", "Toolbar2", "Toolbar3" };
+    private bool m_ShowPoints;
+    private bool m_Value;
+    private float m_SliderValue = 5f;
+    private float m_SliderValueLeft = 0f;
+    private float m_SliderValueRight = 10f;
+
+    private const string k_Point = "m_PointCurrent";
+    private const string k_Points = "m_PointsTemporary";
+    private const string k_PointsConfirmed = "m_PointsConfirmed";
+    private const string k_Waypoints = "m_Waypoints";
+    private const string k_WaypointSystem = "WaypointSystem";
+    private const string k_Toggle = "Toggle";
+    private const string k_AddPoint = "AddPoint";
+    private const string k_ConfirmPoint = "ConfirmPoint";
+    private const string k_ResetPoints = "ResetPoints";
 
     [MenuItem("Tools/WaypointSystem")]
     public static void OpenWaypointSystem()
     {
-        GetWindow<WaypointSystemEditor>("WaypointSystem");
+        GetWindow<WaypointSystemEditor>(k_WaypointSystem);
     }
 
     private void OnEnable()
     {
-        serializedObject = new SerializedObject(this);
-        propertyPoint = serializedObject.FindProperty("point");
+        m_SerializedObject = new SerializedObject(this);
+        m_PropertyPointCurrent = m_SerializedObject.FindProperty(k_Point);
+        m_PropertyPointsTemporary = m_SerializedObject.FindProperty(k_Points);
+        m_PropertyPointsConfirmed = m_SerializedObject.FindProperty(k_PointsConfirmed);
+        m_PropertyWaypoints = m_SerializedObject.FindProperty(k_Waypoints);
         Selection.selectionChanged += Repaint;
         SceneView.duringSceneGui += DuringSceneGUI;
     }
@@ -32,16 +65,97 @@ public class WaypointSystemEditor : EditorWindow
 
     private void OnGUI()
     {
-        serializedObject.Update();
+        m_SerializedObject.Update();
+        EditorGUILayout.PropertyField(m_PropertyPointCurrent);
+        EditorGUILayout.PropertyField(m_PropertyPointsTemporary);
+        EditorGUILayout.PropertyField(m_PropertyPointsConfirmed);
+        EditorGUILayout.PropertyField(m_PropertyWaypoints);
 
-        if (serializedObject.ApplyModifiedProperties())
+        if (m_SerializedObject.ApplyModifiedProperties())
         {
             SceneView.RepaintAll();
+        }
+
+        // if you clicked left mouse button, in the editor window
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            GUI.FocusControl(null);
+            Repaint(); // repaint on the editor window UI
         }
     }
 
     private void DuringSceneGUI(SceneView sceneView)
     {
-
+        if (m_ShowPoints)
+        {
+            foreach (SerializedProperty propertyPoint in m_PropertyPointsTemporary)
+            {
+                propertyPoint.vector3Value = Handles.PositionHandle(propertyPoint.vector3Value, Quaternion.identity);
+            }
+        }
+        Handles.BeginGUI();
+        Rect rectangle = new Rect(8f, 8f, 256f, 64f);
+        m_ToolbarInt = GUI.Toolbar(rectangle, m_ToolbarInt, m_ToolbarStrings);
+        rectangle.position -= Vector2.down * 75;
+        rectangle.width *= 0.5f;
+        if(GUI.Button(rectangle, k_AddPoint))
+        {
+            m_PointsTemporary.Add(m_PropertyPointCurrent.vector3Value);
+            m_ShowPoints = true;
+        }
+        rectangle.position -= Vector2.down * 75;
+        if (GUI.Button(rectangle, k_ConfirmPoint))
+        { 
+            foreach (Vector3 point in m_PointsTemporary)
+            {
+                m_PointsConfirmed.Add(point);
+                bool found = false;
+                foreach (GameObject waypoint in m_Waypoints)
+                {
+                    if (!waypoint.activeSelf)
+                    {
+                        found = true;
+                        waypoint.transform.position = point;
+                        waypoint.SetActive(true);
+                        break;
+                    }
+                }
+                if (found == false)
+                {
+                    GameObject waypoint = Resources.Load<GameObject>("Waypoint");
+                    GameObject currentWaypoint = Instantiate(waypoint, point, Quaternion.identity);
+                    m_Waypoints.Add(currentWaypoint);
+                    currentWaypoint.SetActive(true);
+                }
+            }
+            m_PointsTemporary.Clear();
+            m_ShowPoints = false;
+        }
+        rectangle.position -= Vector2.down * 75;
+        if (GUI.Button(rectangle, k_ResetPoints))
+        {
+            m_PointsConfirmed.Clear();
+            foreach (GameObject waypoint in m_Waypoints)
+            {
+                waypoint.SetActive(false);
+            }
+        }
+        rectangle.position -= Vector2.down * 75;
+        if (GUI.Button(rectangle, "IncreaseWaypoints"))
+        {
+            GameObject waypoint = Resources.Load<GameObject>("Waypoint");
+            for (int i = 0; i < m_SliderValue; i++)
+            {
+                GameObject currentWaypoint = Instantiate(waypoint, Vector3.zero, Quaternion.identity);
+                m_Waypoints.Add(currentWaypoint);
+                currentWaypoint.SetActive(false);
+            }
+        }
+        rectangle.position -= Vector2.down * 75;
+        m_SliderValue = GUI.HorizontalSlider(rectangle, m_SliderValue, m_SliderValueLeft, m_SliderValueRight);
+        rectangle.position -= Vector2.down * 75;
+        m_Value = GUI.Toggle(rectangle, m_Value, k_Toggle);
+        Handles.EndGUI();
+        m_SerializedObject.ApplyModifiedProperties();
     }
 }
